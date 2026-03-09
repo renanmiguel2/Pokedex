40f7579824fba9c9249cdb2ec566dcf2e8289e50
renan_miguel2 <renanmiguel19900@gmail.com>
2026-03-09 12:20:26 -0300

    Ajustes no card unico
    Coloquei a altura e o peso pokemon quando a função é chamada

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card ({ pokemon}) {
     console.log(pokemon)
 
     return (
-        <div className="flex flex-col items-center gap-2 w-40 bg-gray-200 rounded-lg p-4">
+        <div className="flex flex-col items-center gap-2 w-40 bg-gray-200 rounded-lg p-4 w-50 mx-auto hover:scale-105 transition-transform duration-300">
             <h2 className={`font-bold text-lg text-gray-500`}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             <h2 className={`text-md font-semibold text-gray-600 px-2 rounded`}>#
@@ -13,6 +13,14 @@ function Card ({ pokemon}) {
             <h2 className={`text-sm md:text-md font-semibold text-white py-1 px-3 text-center rounded ${typeColors[pokemon.types[0].type.name]} min-w-25`}>
                 {pokemon.types.map((t) => t.type.name).join(", ")}
             </h2>
+            <div className="flex flex-col gap-1 mt-2">
+                <h2>
+                    <span className="font-bold text-gray-500">Altura:</span> {pokemon.height / 10} m
+                </h2>
+                <h2>
+                    <span className="font-bold text-gray-500">Peso:</span> {pokemon.weight / 10} kg
+                </h2>
+            </div>
             </div>
     )
 }
